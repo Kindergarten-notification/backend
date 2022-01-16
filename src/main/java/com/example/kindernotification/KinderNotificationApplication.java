960f90a938021b9b597c7055baac0780c2636b60
ChoiJaeEun <poungki0823@gmail.com>
2022-01-16 14:17:31 +0900

    springboot initialize, setting

---
+++ src/main/java/com/example/kindernotification/KinderNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.kindernotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinderNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinderNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kindernotification/KinderNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kindernotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinderNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
