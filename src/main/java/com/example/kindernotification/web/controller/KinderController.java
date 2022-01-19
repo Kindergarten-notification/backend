b96511b3a074a5e4e905888c4a00b24de758757c
ChoiJaeEun <poungki0823@gmail.com>
2022-01-19 10:44:41 +0900

    kinderService 명 변경, query method 변경

--- src/main/java/com/example/kindernotification/web/controller/KinderController.java
+++ src/main/java/com/example/kindernotification/web/controller/KinderController.java
@@ -1,6 +1,6 @@
 package com.example.kindernotification.web.controller;
 
-import com.example.kindernotification.service.kinder.KinderDetailService;
+import com.example.kindernotification.service.kinder.KinderService;
 import com.example.kindernotification.web.dto.KinderDetailDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class KinderController {
 
-    private final KinderDetailService kinderDetailService;
+    private final KinderService kinderService;
 
     @GetMapping("/kinder/{kinder_id}")
     public KinderDetailDto kinderDetail(@PathVariable(name = "kinder_id") Long id) {
-        return kinderDetailService.findById(id);
+        return kinderService.findById(id);
     }
 
 }
