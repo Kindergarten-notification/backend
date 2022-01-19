ea527b08e71bf7ed186087cc80e0dd3c5b0b0cc9
ChoiJaeEun <poungki0823@gmail.com>
2022-01-19 10:16:45 +0900

    service orElseThrow 추가

--- src/main/java/com/example/kindernotification/service/kinder/KinderService.java
+++ src/main/java/com/example/kindernotification/service/kinder/KinderService.java
@@ -5,16 +5,12 @@ import com.example.kindernotification.domain.kinder.KinderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Service
 public class KinderService {
     private final KinderRepository kinderRepository;
 
     public Kinder findById(Long id) {
-        Optional<Kinder> kinderEntity = kinderRepository.findById(id);
-
-        return kinderEntity.get();
+        return kinderRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(id + " 번 유치원 정보가 없습니다."));
     };
 }
