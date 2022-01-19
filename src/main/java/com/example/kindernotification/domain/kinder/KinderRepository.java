df6a605c536c5318f77525be5e6cdc34fa1135a5
ChoiJaeEun <poungki0823@gmail.com>
2022-01-19 10:43:13 +0900

    Query 생성

--- src/main/java/com/example/kindernotification/domain/kinder/KinderRepository.java
+++ src/main/java/com/example/kindernotification/domain/kinder/KinderRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface KinderRepository extends JpaRepository<Kinder, Long> {
-    @Query
-    List<Kinder> findAllDesc();
+    @Query(value = "SELECT * FROM kinder as k ORDER BY id DESC", nativeQuery = true)
+    List<Kinder> findAllOrderByIdDesc();
 }
 
