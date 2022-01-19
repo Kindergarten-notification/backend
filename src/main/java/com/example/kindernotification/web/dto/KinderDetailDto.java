8bea73f81dbed4ee085aac4d61c43c6e65ff4c2a
ChoiJaeEun <poungki0823@gmail.com>
2022-01-19 10:42:13 +0900

    dto 에 JsonNaming 추가

--- src/main/java/com/example/kindernotification/web/dto/KinderDetailDto.java
+++ src/main/java/com/example/kindernotification/web/dto/KinderDetailDto.java
@@ -1,10 +1,13 @@
 package com.example.kindernotification.web.dto;
 
 import com.example.kindernotification.domain.kinder.Kinder;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @Data
 @NoArgsConstructor
 public class KinderDetailDto {
