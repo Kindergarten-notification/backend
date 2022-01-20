7c422a285b4418ac058590bc402c1ba603f59b29
천명준 <mjforwarder7411@gmail.com>
2022-01-20 14:59:15 +0900

    게시글 전체 목록 내림차순 조회

--- src/main/java/com/example/kindernotification/domain/post/PostRepository.java
+++ src/main/java/com/example/kindernotification/domain/post/PostRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     // 특정 유치원 게시글 모두 조회
-    @Query(value = "SELECT * FROM post WHERE KINDER_ID = :kinderCode", nativeQuery = true)
+    @Query(value = "SELECT * FROM post WHERE KINDER_ID = :kinderCode DESC", nativeQuery = true)
     List<Post> findKinderPostSelect(Long kinderCode);
 
     // 특정 유치원 특정 게시글 조회
