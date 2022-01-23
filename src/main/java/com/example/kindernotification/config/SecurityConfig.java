5b9a71a47c6730f6a0881f3c93af817813ec841c
ChoiJaeEun <poungki0823@gmail.com>
2022-01-23 23:23:13 +0900

    security 설정 복구

--- src/main/java/com/example/kindernotification/config/SecurityConfig.java
+++ src/main/java/com/example/kindernotification/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.web.filter.CorsFilter;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)  // @Secured annotation 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    private final CorsFilter corsFilter;
+    private final CorsFilter corsFilter;
     private final UserService userService;
 
     /** 최재은 1/18 (화)
@@ -28,20 +28,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      *   - AuthenticationManager 를 parameter 로 전달 (WebSecurityConfigurerAdapter 가 들고 있음)
      *
      * */
-    /*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // SecurityContextPersistenceFilter: Security chain 에서 가장 먼저 실행되는 Filter
-         http.addFilterBefore(new JwtAuthenticationFilter(authenticationManager()), SecurityContextPersistenceFilter.class);
-        http.addFilterBefore(new JwtAuthorizationFilter(authenticationManager(), userService), SecurityContextPersistenceFilter.class);
+//         http.addFilterBefore(new JwtAuthenticationFilter(authenticationManager()), SecurityContextPersistenceFilter.class);
+//        http.addFilterBefore(new JwtAuthorizationFilter(authenticationManager(), userService), SecurityContextPersistenceFilter.class);
         http.csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // session 사용 안함
             .and()
-//            .addFilter(corsFilter)
+            .addFilter(corsFilter)
             .formLogin().disable()
             .httpBasic().disable()
-//            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-//                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userService))
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userService))
             .authorizeRequests()
                     .antMatchers("/api/user/**")
                         .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
@@ -51,38 +50,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .access("hasRole('ROLE_ADMIN')")
                                 .anyRequest().permitAll()
             .and().exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
-    }
-     */
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                //csrf 토큰 비활성화, 시큐리티에서 js로 요청이오면 csft토큰이 없어서 막아버리는데, disable로 해결
-                .csrf().disable()
-                .exceptionHandling()
-//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint()) // 401
-//                .accessDeniedHandler(jwtAuthenticationEntryPoint) // 403 user -> adminPage access denied
-
-                .and()
-                .headers()
-                .frameOptions()
-                .sameOrigin()
-
-                // 세션을 사용하기 않기에 세션 설정을 STATELESS로 지정
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                .and()
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/api/hello").permitAll()
-                .antMatchers("/api/login").permitAll()
-                .antMatchers("/api/signup").permitAll()
-                .antMatchers("/api/books/{bookId}/detail").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login");
     }
 }
